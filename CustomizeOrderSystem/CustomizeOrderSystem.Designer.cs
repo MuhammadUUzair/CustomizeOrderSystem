2e8f61d4754a5e6c6c1486996a29a4644c008da6
MuhammadUUzair <muhammaduuzair22@gmail.com>
2023-03-22 03:15:06 -0700

    Add project files.

---
+++ CustomizeOrderSystem/CustomizeOrderSystem.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace CustomizeOrderSystem
+{
+    partial class CustomizeOrderSystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_OrderNo = new System.Windows.Forms.TextBox();
+            this.txt_OrderDetail = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btn_INSERT = new System.Windows.Forms.Button();
+            this.btn_SHOWDATA = new System.Windows.Forms.Button();
+            this.btn_DELETE = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_OrderNo
+            // 
+            this.txt_OrderNo.Location = new System.Drawing.Point(111, 96);
+            this.txt_OrderNo.Name = "txt_OrderNo";
+            this.txt_OrderNo.Size = new System.Drawing.Size(151, 20);
+            this.txt_OrderNo.TabIndex = 0;
+            // 
+            // txt_OrderDetail
+            // 
+            this.txt_OrderDetail.Location = new System.Drawing.Point(111, 138);
+            this.txt_OrderDetail.Name = "txt_OrderDetail";
+            this.txt_OrderDetail.Size = new System.Drawing.Size(151, 20);
+            this.txt_OrderDetail.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(296, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "CUSTOMIZE ORDER SYSTEM";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "OrderDetail : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 16);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "OrderNo :";
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(38, 216);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(433, 170);
+            this.dgv.TabIndex = 6;
+            this.dgv.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellClick);
+            // 
+            // btn_INSERT
+            // 
+            this.btn_INSERT.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_INSERT.Location = new System.Drawing.Point(525, 135);
+            this.btn_INSERT.Name = "btn_INSERT";
+            this.btn_INSERT.Size = new System.Drawing.Size(93, 23);
+            this.btn_INSERT.TabIndex = 3;
+            this.btn_INSERT.Text = "INSERT";
+            this.btn_INSERT.UseVisualStyleBackColor = false;
+            this.btn_INSERT.Click += new System.EventHandler(this.btn_INSERT_Click);
+            // 
+            // btn_SHOWDATA
+            // 
+            this.btn_SHOWDATA.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.btn_SHOWDATA.ForeColor = System.Drawing.Color.Black;
+            this.btn_SHOWDATA.Location = new System.Drawing.Point(525, 97);
+            this.btn_SHOWDATA.Name = "btn_SHOWDATA";
+            this.btn_SHOWDATA.Size = new System.Drawing.Size(93, 23);
+            this.btn_SHOWDATA.TabIndex = 2;
+            this.btn_SHOWDATA.Text = "SHOW DATA";
+            this.btn_SHOWDATA.UseVisualStyleBackColor = false;
+            this.btn_SHOWDATA.Click += new System.EventHandler(this.btn_SHOWDATA_Click);
+            // 
+            // btn_DELETE
+            // 
+            this.btn_DELETE.BackColor = System.Drawing.Color.Red;
+            this.btn_DELETE.Location = new System.Drawing.Point(525, 173);
+            this.btn_DELETE.Name = "btn_DELETE";
+            this.btn_DELETE.Size = new System.Drawing.Size(93, 23);
+            this.btn_DELETE.TabIndex = 4;
+            this.btn_DELETE.Text = "DELETE";
+            this.btn_DELETE.UseVisualStyleBackColor = false;
+            this.btn_DELETE.Click += new System.EventHandler(this.btn_DELETE_Click);
+            // 
+            // CustomizeOrderSystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Turquoise;
+            this.ClientSize = new System.Drawing.Size(658, 413);
+            this.Controls.Add(this.btn_DELETE);
+            this.Controls.Add(this.btn_SHOWDATA);
+            this.Controls.Add(this.btn_INSERT);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_OrderDetail);
+            this.Controls.Add(this.txt_OrderNo);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Name = "CustomizeOrderSystem";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CustomizeOrderSystem";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_OrderNo;
+        private System.Windows.Forms.TextBox txt_OrderDetail;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btn_INSERT;
+        private System.Windows.Forms.Button btn_SHOWDATA;
+        private System.Windows.Forms.Button btn_DELETE;
+    }
+}
---
+++ CustomizeOrderSystem/CustomizeOrderSystem.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomizeOrderSystem
+{
+    public partial class CustomizeOrderSystem : Form
+    {
+        static SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-J7UC1JC\SQLEXPRESS;Initial Catalog=CustomizeOrderSystem;Integrated Security=True");
+        static SqlCommand scmd;
+        static int getid;
+        public CustomizeOrderSystem()
+        {
+            InitializeComponent();
+            display();
+        } 
+
+        private void btn_INSERT_Click(object sender, EventArgs e)
+        {
+            if (!authenticate())
+            {
+                MessageBox.Show("PLZ FILL BOX");
+                return;
+
+            }
+            con.Open();
+            string query = "insert into SystemTable (OrderNo,OrderDetail) values(@OrderNo,@OrderDetail)";
+            scmd = new SqlCommand(query, con);
+            scmd.Parameters.AddWithValue("OrderNo", Convert.ToInt32(txt_OrderNo.Text));
+            scmd.Parameters.AddWithValue("OrderDetail", txt_OrderDetail.Text);
+            scmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Record added sucessfully");
+            display();
+
+        }
+        bool authenticate()
+        {
+            if (string.IsNullOrWhiteSpace(txt_OrderNo.Text) || (string.IsNullOrWhiteSpace(txt_OrderDetail.Text)))
+                return false;
+
+            else
+                return true;
+
+        }
+
+        private void btn_SHOWDATA_Click(object sender, EventArgs e)
+        {
+            display();
+        }
+
+        void display()
+        {
+            con.Open();
+            string query = "select * from SystemTable";
+            SqlDataAdapter sda = new SqlDataAdapter(query, con);
+            DataTable dt1 = new DataTable();
+            sda.Fill(dt1);
+            dgv.DataSource = dt1;
+            con.Close();
+        }
+
+        private void btn_DELETE_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string query = "delete from SystemTable where id=@getid";
+            scmd = new SqlCommand(query, con);
+            scmd.Parameters.AddWithValue("getid", getid);
+            
+            scmd.ExecuteNonQuery();
+            con.Close();
+
+            MessageBox.Show("Record deleted sucessfully");
+        }
+
+        private void dgv_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            getid = Convert.ToInt32(dgv.Rows[e.RowIndex].Cells["ID"].Value);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
